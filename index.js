0e5c1c1efafc9c26bb58124640396858d0515051
ParbhatKataria <parbhatkataria628@gmail.com>
2023-05-09 00:46:23 +0530

    done

--- index.js
+++ index.js
@@ -1,56 +1,69 @@
-const app = require('express')();
-const cors = require('cors');
+const express = require("express");
+const app = express()
+const cors = require("cors");
 app.use(cors());
-const mongoose = require('mongoose');
-const { WasteModel } = require('./model/waste.model');
-const connection = mongoose.connect('mongodb+srv://parbhat:parbhat@cluster0.lansvyo.mongodb.net/waste?retryWrites=true&w=majority')
+const mongoose = require("mongoose");
+const { WasteModel } = require("./model/waste.model");
+const connection = mongoose.connect(
+  "mongodb+srv://parbhat:parbhat@cluster0.lansvyo.mongodb.net/waste?retryWrites=true&w=majority"
+);
 
-app.get('/', async(req,res)=>{
-    try {
-        let data = await WasteModel.find();
-        res.send(data);
-    } catch (error) {
-        res.send('data not found')
-    }
+app.use(express.json());
+
+app.use((req, res, next)=>{
+    console.log(req.body, 'middle');
+    next()
 })
 
-app.post('/', async(req,res)=>{
-    let item = req.body;
-    try {
-        let data = WasteModel({...item});
-        await data.save();
-        res.send('data is stored');
-    } catch (error) {
-        res.send('data is not able to store');
-    }
-})
+app.get("/", async (req, res) => {
+  try {
+    let data = await WasteModel.find();
+    res.send(data);
+  } catch (error) {
+    res.send("data not found");
+  }
+});
 
-app.patch('/:id', async(req,res)=>{
-    let item = req.body;
+app.post("/", async (req, res) => {
+  let item = req.body;
+  let id = req.headers.authorization;
+  console.log(item, id);
+  try {
+    let data = WasteModel({...item});
+    await data.save();
+    res.send("data is stored");
+  } catch (error) {
+    res.send("data is not able to store");
+  }
+});
+
+app.patch("/:id", async (req, res) => {
+  let item = req.body;
+  let id = req.params.id;
+  console.log(item, id)
+  try {
+    let data = await WasteModel.findByIdAndUpdate(id, item, {new:true});
+    res.send("data");
+  } catch (error) {
+    res.send("data is not updated");
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  try {
     let id = req.params.id;
-    try {
-        let data = await WasteModel.findByIdAndUpdate(id, item, {new:true});
-        res.send(data);
-    } catch (error) {
-        res.send('data is not updated');
-    }
-})
+    let data = await WasteModel.findByIdAndDelete(id, { new: true });
+    res.send(data);
+  } catch (error) {
+    res.send("data is not deleted");
+  }
+});
 
-app.delete('/:id', async(req,res)=>{
-    try {
-        let id = req.params.id;
-        let data = await WasteModel.findByIdAndDelete(id, {new:true});
-        res.send(data);
-    } catch (error) {
-        res.send('data is not deleted')
-    }
-})
-
-app.listen(4500, async(req, res)=>{
-    try {
-        await connection;
-        console.log('database is running')
-    } catch (error) {
-        console.log('database is not running')
-    }
-})
+app.listen(4500, async (req, res) => {
+  try {
+    await connection;
+    console.log("database is running");
+  } catch (error) {
+    console.log("database is not running");
+  }
+});
